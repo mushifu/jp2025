3d525906bec75d1c92a1659bfa657be28d18ea46
AlejandroAN <almetrash@gmail.com>
2025-07-19 20:09:28 +0200

    + Added custom markers for spot types

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -12,6 +12,42 @@ const center = {
   lng: 139.6503
 };
 
+//  Mapeo de tipos a iconos y tamaños
+const markerStyles = {
+  restaurant: {
+    url: "/icons/restaurant.png",
+    size: [32, 32]
+  },
+  hotel: {
+    url: "/icons/hotel.png",
+    size: [32, 32]
+  },
+  monumento: {
+    url: "/icons/monumento.png",
+    size: [32, 32]
+  },
+  templo: {
+    url: "/icons/templos.png",
+    size: [32, 32]
+  },
+  fotos: {
+    url: "/icons/fotos.png",
+    size: [32, 32]
+  },
+  compras: {
+    url: "/icons/compras.png",
+    size: [32, 32]
+  },
+  ocio: {
+    url: "/icons/ocio.png",
+    size: [32, 32]
+  },
+  default: {
+    url: "/icons/default.png",
+    size: [32, 32]
+  }
+};
+
 const Map = () => {
   const [markers, setMarkers] = useState([]);
 
@@ -38,21 +74,25 @@ const Map = () => {
   }, [loadMarkers]);
 
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={5}
-    >
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={5}>
       <MarkerClustererF>
         {(clusterer) =>
-          markers.map((marker, idx) => (
-            <Marker
-              key={idx}
-              position={{ lat: marker.lat, lng: marker.lng }}
-              title={marker.title || ""}
-              clusterer={clusterer}
-            />
-          ))
+          markers.map((marker, idx) => {
+            const style = markerStyles[marker.type] || markerStyles.default;
+
+            return (
+              <Marker
+                key={idx}
+                position={{ lat: marker.lat, lng: marker.lng }}
+                title={marker.title || ""}
+                clusterer={clusterer}
+                icon={{
+                  url: style.url,
+                  scaledSize: new window.google.maps.Size(...style.size)
+                }}
+              />
+            );
+          })
         }
       </MarkerClustererF>
     </GoogleMap>
