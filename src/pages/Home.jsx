2b3d3b1e47b0cf990f3314bb64bfc2a7161150b5
AlejandroAN <almetrash@gmail.com>
2025-07-27 08:31:26 +0200

    + Added styles for dynamic viewport that fixes unnecessary vertical scroll on mobile + Moved style definitions to index.css for animation in map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,7 +89,7 @@ export default function App() {
 
   return (
       <BackgroundWrapper>
-    <div>
+    <div className="h-[100dvh] flex flex-col justify-between">
       <CurrencyConverter />
       <ClockDisplay
         city={displayedCity}
